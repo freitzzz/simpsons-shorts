ab98cc67688be5c492512280b87d8e12ab489316
freitzzz <1160907@isep.ipp.pt>
2023-10-25 19:56:05 +0100

    feat: init lumberkodee and log button click

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,15 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.Dp
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import com.github.kotlinartisans.lumberkodee.logDebug
+import com.github.kotlinartisans.lumberkodee.putLumberkodeeToWork
+import logging.SimpsonsLumberkodeeClient
 
 @Composable
 @Preview
@@ -22,6 +20,7 @@ fun App() {
     MaterialTheme {
         Button(onClick = {
             text = "Hello, Desktop!"
+            logDebug("button was clicked")
         }) {
             Text(text)
         }
@@ -29,6 +28,12 @@ fun App() {
 }
 
 fun main() = application {
+    putLumberkodeeToWork(
+        arrayListOf(
+            SimpsonsLumberkodeeClient()
+        )
+    )
+
     Window(
         resizable = false,
         state = WindowState(
