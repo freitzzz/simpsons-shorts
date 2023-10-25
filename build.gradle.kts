87caead8492c4d9f797cc0037ed1cedc33cf3452
freitzzz <1160907@isep.ipp.pt>
2023-10-25 19:23:37 +0100

    chore(deps): bump kotlin 1.9.10 and compose 1.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.compose") version "1.5.3"
 }
 
 group = "com.github.freitzzz"
