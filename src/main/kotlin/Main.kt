4211ad218ce15882456de1767c0fe70c82c9e424
freitzzz <1160907@isep.ipp.pt>
2023-10-28 11:22:52 +0100

    feat: present list view with available shorts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -10,19 +13,45 @@ import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.github.kotlinartisans.lumberkodee.logDebug
 import com.github.kotlinartisans.lumberkodee.putLumberkodeeToWork
+import core.ReactorComposer
+import core.Vault
+import data.clients.CinderelaNetworkingClient
+import data.repositories.CinderelaSimpsonsShortsRepository
+import data.repositories.SimpsonsShortsRepository
 import logging.SimpsonsLumberkodeeClient
+import state.shorts.QueryAllShortsEvent
+import state.shorts.ShortsReactor
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    vault: Vault,
+) {
     var text by remember { mutableStateOf("Hello, World!") }
+    val shortsReactor = ShortsReactor(vault.read())
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-            logDebug("button was clicked")
-        }) {
-            Text(text)
+        Column {
+            Button(
+                onClick = {
+                    text = "Hello, Desktop!"
+                    logDebug("button was clicked")
+
+                    shortsReactor.add(QueryAllShortsEvent())
+                }) {
+                Text(text)
+            }
+
+            ReactorComposer(
+                shortsReactor,
+            ) {
+                LazyColumn {
+                    items(it) {
+                        println("!!!")
+                        Text(it)
+                    }
+                }
+            }
         }
     }
 }
@@ -34,13 +63,23 @@ fun main() = application {
         )
     )
 
+    val vault = createVault()
+
     Window(
         resizable = false,
         state = WindowState(
-            size = DpSize(600.dp, 400.dp)
+            size = DpSize(600.dp, 400.dp),
         ),
         onCloseRequest = ::exitApplication
     ) {
-        App()
+        App(vault)
     }
+}
+
+private fun createVault() = Vault.empty().apply {
+    store<SimpsonsShortsRepository>(
+        CinderelaSimpsonsShortsRepository(
+            CinderelaNetworkingClient()
+        )
+    )
 }
